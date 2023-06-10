7f75d7510d6edf6da2d8a991af724793629774c1
Jose Arias <115059715+Monoarias777@users.noreply.github.com>
2023-06-10 13:54:52 -0400

    Phase1

--- src/index.js
+++ src/index.js
@@ -1,9 +1,4 @@
 console.log("Starting with Back-end already.")
 
-import {add,} from './operations.js' /*Import from the 1 Module */
-import {deliveryResult1, deliveryResult2, deliveryResult3 } from './conditionals.js' /*Import from the 2 Module */
-console.log(add(2, 4));
-console.log(deliveryResult1);
-console.log(deliveryResult2);
-console.log(deliveryResult3);
+
 
